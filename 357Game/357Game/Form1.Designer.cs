f94e3fcdcfcefd4664d75363d6b1c586db4599a4
Neil-kingwolf <82758755+Neil-kingwolf@users.noreply.github.com>
2021-04-18 19:44:21 +0800

    Add files via upload

---
+++ 357Game/357Game/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _357Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInfo = new System.Windows.Forms.RichTextBox();
+            this.btnBegin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(66, 182);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(625, 197);
+            this.txtInfo.TabIndex = 0;
+            this.txtInfo.Text = "";
+            // 
+            // btnBegin
+            // 
+            this.btnBegin.Location = new System.Drawing.Point(66, 28);
+            this.btnBegin.Name = "btnBegin";
+            this.btnBegin.Size = new System.Drawing.Size(75, 23);
+            this.btnBegin.TabIndex = 1;
+            this.btnBegin.Text = "开始";
+            this.btnBegin.UseVisualStyleBackColor = true;
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBegin);
+            this.Controls.Add(this.txtInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtInfo;
+        private System.Windows.Forms.Button btnBegin;
+    }
+}
+
